5bd359bc522ddd3f268c0a9d886dd82404038575
Mahesh Auti <mahesh.auti@cuelogic.com>
2018-01-30 14:09:22 +0530

    stable code

--- javascript/profileModule.js
+++ javascript/profileModule.js
@@ -37,18 +37,24 @@ function makeEditable() {
     // var para = document.createElement("input");
     // para.setAttribute("type", "file");
     // para.setAttribute("style", "display: block;");
-    var parent = document.getElementById("choose");
-    parent.style.display = "block;"
+    // var parent = document.getElementById("choose");
+    // parent.style.display = "block;"
     // parent.appendChild(para);
 }
 
 function updateInfo() {
+     username = getUser();
+    var allEntries = getArray();
+    var x = search(username, allEntries);
+    obj = allEntries[x];
     obj.Fname = document.getElementById("PR_FN").value;
     obj.Lname = document.getElementById("PR_LN").value;
     obj.gender = document.getElementById("PR_GENDER").value;
     obj.Address = document.getElementById("PR_ADDRESS").value;
     obj.imgurl = document.getElementById("PR_IMAGE").value;
+    
     setArray(allEntries);
+
     window.alert("profile updated successfully");
 
 }
